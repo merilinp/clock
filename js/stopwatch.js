a2f087b0b06ad54bbdd4b9c1962af286fd8a7320
merilinp <merilinpriidik99@gmail.com>
2021-11-06 14:30:39 +0200

    timer

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -17,33 +17,33 @@ setInterval(refreshTime, 1000);
 
 // STOPWATCH
 
-//Define vars to hold time values
+//Vars to hold time values
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-//Define vars to hold "display" value
+//Vars to hold "display" value
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
 
-//Define var to hold setInterval() function
+//Var to hold setInterval() function
 let interval = null;
 
-//Define var to hold stopwatch status
+//Var to hold stopwatch status
 let staatus = "stopped";
 
-//Stopwatch function (logic to determine when to increment next value, etc.)
-function stopWatch(){
+//Stopwatch function (logic to determine when to increment next value)
+function stopWatch() {
 
     seconds++;
 
     //Logic to determine when to increment next value
-    if(seconds / 60 === 1){
+    if (seconds / 60 === 1) {
         seconds = 0;
         minutes++;
 
-        if(minutes / 60 === 1){
+        if (minutes / 60 === 1) {
             minutes = 0;
             hours++;
         }
@@ -51,45 +51,44 @@ function stopWatch(){
     }
 
     //If seconds/minutes/hours are only one digit, add a leading 0 to the value
-    if(seconds < 10){
+    if (seconds < 10) {
         displaySeconds = "0" + seconds.toString();
     }
-    else{
+    else {
         displaySeconds = seconds;
     }
 
-    if(minutes < 10){
+    if (minutes < 10) {
         displayMinutes = "0" + minutes.toString();
     }
-    else{
+    else {
         displayMinutes = minutes;
     }
 
-    if(hours < 10){
+    if (hours < 10) {
         displayHours = "0" + hours.toString();
     }
-    else{
+    else {
         displayHours = hours;
     }
 
     //Display updated time values to user
-    document.getElementById("display").innerHTML = displayHours + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + displayMinutes + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + displaySeconds;
-
+    document.getElementById("hour").innerHTML = displayHours;
+    document.getElementById("minute").innerHTML = displayMinutes;
+    document.getElementById("second").innerHTML = displaySeconds;
+    
 }
 
+function startStop() {
 
-
-function startStop(){
-
-    if(staatus === "stopped"){
+    if (staatus === "stopped") {
 
         //Start the stopwatch (by calling the setInterval() function)
         interval = window.setInterval(stopWatch, 1000);
         document.getElementById("startStop").innerHTML = "stop";
         staatus = "started";
-
     }
-    else{
+    else {
 
         window.clearInterval(interval);
         document.getElementById("startStop").innerHTML = "resume";
@@ -100,13 +99,15 @@ function startStop(){
 }
 
 //Function to reset the stopwatch
-function reset(){
+function reset() {
 
     window.clearInterval(interval);
     seconds = 0;
     minutes = 0;
     hours = 0;
-    document.getElementById("display").innerHTML = "00&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;00&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;00";
+    document.getElementById("hour").innerHTML = "00";
+    document.getElementById("minute").innerHTML = "00";
+    document.getElementById("second").innerHTML = "00";
     document.getElementById("startStop").innerHTML = "start";
 
 }
--- js/timer.js
+++ js/timer.js
@@ -17,3 +17,114 @@ setInterval(refreshTime, 1000);
 
 // TIMER
 
+var countdownTimer;
+
+//Vars to hold "display" value
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
+//Var to hold timer status
+let staatus = "stopped";
+
+//Timer
+function timer(endTime) {
+
+    var now = new Date();
+
+    //Finding time difference
+    if (endTime.getTime() - now.getTime() > 0) {
+
+        var difference = endTime - now;
+
+        var hh = Math.floor(difference / 3600000);
+        var mm = Math.floor((difference % 3600000) / 60000);
+        var ss = Math.round(((difference % 3600000) % 60000) / (1000));
+
+        //If seconds/minutes/hours are only one digit, add a leading 0 to the value
+        if (ss < 10) {
+            displaySeconds = "0" + ss.toString();
+        }
+        else {
+            displaySeconds = ss;
+        }
+
+        if (mm < 10) {
+            displayMinutes = "0" + mm.toString();
+        }
+        else {
+            displayMinutes = mm;
+        }
+
+        if (hh < 10) {
+            displayHours = "0" + hh.toString();
+        }
+        else {
+            displayHours = hh;
+        }
+
+        //Display updated time values to user
+        document.getElementById("hour").value = displayHours;
+        document.getElementById("minute").value = displayMinutes;
+        document.getElementById("second").value = displaySeconds;
+
+
+    } else {
+        document.getElementById("hour").value = "00";
+        document.getElementById("minute").value = "00";
+        document.getElementById("second").value = "00";
+        // document.getElementById("message").innerHTML = "Timer expired";
+        document.getElementById("startStop").innerHTML = "start";
+        clearInterval(countdownTimer);
+    }
+}
+
+function startStop() {
+
+    if (staatus === "stopped") {
+
+        var startTime = new Date();
+
+        var h = parseInt(document.getElementById("hour").value, 0);
+        var m = parseInt(document.getElementById("minute").value, 0);
+        var s = parseInt(document.getElementById("second").value, 0);
+
+        //Check if entered numbers are positive
+        if (h > 0 || m > 0 || s > 0) {
+
+            var endTime = new Date(startTime.getTime() + (s * 1000 + m * 60000 + h * 3600000));
+
+            //Call timer function
+            countdownTimer = setInterval(function () {
+                timer(endTime);
+            }, 1000);
+            
+        }
+        else {
+            //ERROR 
+            alert("Something went wrong! Check the entered numbers.");
+        }
+
+        document.getElementById("startStop").innerHTML = "stop";
+        staatus = "started";
+
+    }
+    else {
+
+        window.clearInterval(countdownTimer);
+        document.getElementById("startStop").innerHTML = "resume";
+        staatus = "stopped";
+
+    }
+
+}
+
+//Function to reset the timer
+function reset() {
+    window.clearInterval(countdownTimer);
+    document.getElementById("hour").value = "00";
+    document.getElementById("minute").value = "00";
+    document.getElementById("second").value = "00";
+    document.getElementById("startStop").innerHTML = "start";
+    staatus = "stopped";
+}
